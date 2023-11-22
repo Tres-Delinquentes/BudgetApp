1dc0b36f6911982e889837f842df4591ea7f7509
Jesper Misiorny <jesper.keden@hotmail.com>
2023-11-22 14:04:12 +0100

    Added tests for creating a new Item, errorhandling, exceptions and some logic to CreateItem

--- Backend/DAL/ItemManager.cs
+++ Backend/DAL/ItemManager.cs
@@ -1,18 +1,61 @@
-﻿namespace Backend.DAL
+﻿using Backend.Models;
+using System.Text.RegularExpressions;
+
+namespace Backend.DAL
 {
     public class ItemManager
     {
-        public void CreateSubCategory()
+        public ItemManager()
+        {
+            
+        }
+
+        public Item CreateItem(string name, float amount) //string amount
+        {
+
+            // Trim leading and trailing whitespaces
+            name = name?.Trim();
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be null, empty, or whitespace.");
+            }
+
+            if (name.Length > 50)
+            {
+                throw new ArgumentException("Name cannot be longer then 50 characters.");
+            }
+
+            if (!char.IsLetterOrDigit(name[0]))
+            {
+                throw new ArgumentException("Name cannot start with a special character.");
+            }
+
+            // Regex: Each word must start with an alphanumeric character, underscore, or dash.
+            // This allows for whitespace to be inside the string, but not have leading/trailing due to Trim();
+            Regex validNameRegex = new Regex(@"^[a-zA-Z0-9-_]+( [a-zA-Z0-9-_]+)*$");           
+            if(!validNameRegex.IsMatch(name))
+            {
+                throw new ArgumentException("Name contains invalid characters.");
+            }
+
+            // Gör om string till float ifall vi väljer den vägen.
+            //float amount = ParseAmount(amountString);
+
+            if (amount < 0)
+            {
+                throw new ArgumentException("Amount cannot be a negative number.");
+            }
+
+            return new Item { Name = name, Amount = amount };
+        }
+
+        public void DeleteItem()
         {
 
         }
 
-        public void DeleteSubCategory()
-        {
-
-        }
-
-        public void EditSubCategory()
+        public void EditItem()
         {
 
         }
--- Backend/Models/Item.cs
+++ Backend/Models/Item.cs
@@ -1,11 +1,29 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace Backend.Models
 {
     public class Item
     {
+        public Guid Id { get; set; } = new Guid();
         public string Name { get; set; }
-        public float Amount { get; set; }        
+        public float Amount { get; set; } 
+
+
+        // Ska float Amount vara en string för bättre flexibilitet?
+        // Vi måste validera ordentligt då.
+
+        //public float ParseAmount(string amountString)
+        //{
+        //    try
+        //    {
+        //        return float.Parse(amountString, CultureInfo.InvariantCulture);
+        //    }
+        //    catch (FormatException)
+        //    {                
+        //        throw new ArgumentException("Amount is not in a valid format");
+        //    }
+        //}
 
     }
 }
