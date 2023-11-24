3b9aa764489c06ac34ba3a5de429cb577c2719f3
Andreas Tollmar <andreastollmar@gmail.com>
2023-11-24 08:59:03 +0100

    display budget in frontend

--- frontend/src/Components/FetchBudget.svelte
+++ frontend/src/Components/FetchBudget.svelte
@@ -1,21 +1,31 @@
 <script>
   import { onMount } from "svelte";
-  let data2;
-  let budget;
-  let rent;
+  let budget = { expenses: [], income: [] };
 
   onMount(async () => {
     const response = await fetch("https://localhost:7022/api/Budget");
     console.log(response);
-    const data = await response.json();
-    budget = data;
-    console.log(budget);
-    console.log(budget.expenses[0].items[0].name);
-    rent = budget.expenses.map((x) => x.name);
-    console.log(rent);
+    if (response.ok) {
+      const data = await response.json();
+      budget = data;
+    } else {
+      console.error("Failed to fetch budget:", response.status);
+    }
   });
 </script>
 
 <main>
-  <p>{rent}</p>
+  {#each budget.expenses as expense}
+    <div class="expense">
+      <h3>{expense.name}</h3>
+      <div class="items">
+        {#each expense.items as item}
+          <p>{item.name}: {item.amount}</p>
+        {/each}
+      </div>
+    </div>
+  {/each}
+  {#each budget.income as income}
+    <p>{income.name}: {income.amount}</p>
+  {/each}
 </main>
