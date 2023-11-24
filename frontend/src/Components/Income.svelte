59d8b573558845905dbd60238085c6fce346459e
Gr2dd3 <gradde23@gmail.com>
2023-11-24 11:39:55 +0100

    Working on ogranizing and displaying budget on f-e

---
+++ frontend/src/Components/Income.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let budget;
+    let currency = "SEK";
+</script>
+
+<section>
+    {#each budget.income as income}
+        <p>{income.name}: {income.amount}</p>
+    {/each}
+</section>
