e8a43570da93195ee0fcb86db2c91a71875e7f92
Gr2dd3 <gradde23@gmail.com>
2023-11-24 14:29:45 +0100

    Added items to smallBudget

--- Backend/Helpers/BudgetFiller.cs
+++ Backend/Helpers/BudgetFiller.cs
@@ -4,6 +4,7 @@ namespace Backend.Helpers
 {
     internal class BudgetFiller
     {
+        private List<List<Item>> ItemLists = new List<List<Item>>();
         internal Budget? FetchLargeBudget()
         {
             var budget = new Budget();
@@ -38,21 +39,65 @@ namespace Backend.Helpers
 
         private List<Category> CreateCategoriesForSmallBudget()
         {
-            var items = new List<Item>()
-            {
-                new Item { Id = 1, Name = "Hyra", Amount = 100 }
-            };
+
+            GetItemLists();
 
             var categories = new List<Category>();
-            categories.Add(new Category() { Name = "Boende", Items = items });
-            categories.Add(new Category() { Name = "Mat" });
-            categories.Add(new Category() { Name = "Transport" });
-            categories.Add(new Category() { Name = "Hälsa" });
-            categories.Add(new Category() { Name = "Skulder & Lån" });
+
+            if (ItemLists != null && ItemLists.Count >= 4)
+            {
+                categories.Add(new Category() { Name = "Boende", Items = ItemLists[0] });
+                categories.Add(new Category() { Name = "Mat", Items = ItemLists[1] });
+                categories.Add(new Category() { Name = "Transport", Items = ItemLists[2] });
+                categories.Add(new Category() { Name = "Hälsa", Items = ItemLists[3] });
+                categories.Add(new Category() { Name = "Skulder & Lån", Items = ItemLists[4] });
+            }
+            else
+            {
+                categories.Add(new Category() { Name = "Boende" });
+                categories.Add(new Category() { Name = "Mat" });
+                categories.Add(new Category() { Name = "Transport" });
+                categories.Add(new Category() { Name = "Hälsa" });
+                categories.Add(new Category() { Name = "Skulder & Lån" });
+            }
 
 
             return categories;
 
+        }
+
+        private void GetItemLists()
+        {
+
+            var boendeItems = new List<Item>()
+            {
+                new Item() { Name = "Hyra", Amount = 7500 },
+                new Item() { Name = "El", Amount = 1250 },
+                new Item() { Name = "Internet", Amount = 250 }
+            };
+            var matItems = new List<Item>()
+            {
+                new Item() { Name = "Inköp månad", Amount = 6000 },
+                new Item() { Name = "Inköp vecka", Amount = 500 }
+            };
+            var transportItems = new List<Item>()
+            {
+                new Item() { Name = "Bensin", Amount = 3600 }
+            };
+            var halsaItems = new List<Item>()
+            {
+                new Item() { Name = "Gym", Amount = 0 }
+            };
+            var skulderItems = new List<Item>()
+            {
+                new Item() { Name = "Stora lånet", Amount = 2700 }
+            };
+
+            ItemLists.Add(boendeItems);
+            ItemLists.Add(matItems);
+            ItemLists.Add(transportItems);
+            ItemLists.Add(halsaItems);
+            ItemLists.Add(skulderItems);
         }
 
         private List<Category> CreateCategoriesForMediumBudget()
