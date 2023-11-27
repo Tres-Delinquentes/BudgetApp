d35a8e0f0f8d8ec4e200d32fb000cf0c833be42c
Jesper Misiorny <jesper.keden@hotmail.com>
2023-11-27 13:39:45 +0100

    Darkmode init

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,18 +4,28 @@
   import Counter from './lib/Counter.svelte'
   import FetchBudget from './Components/FetchBudget.svelte'
   import Button from './Components/Button.svelte'
-    import Nav from './Components/Nav.svelte';
-    const name = 'Mattias';
+  import Nav from './Components/Nav.svelte'
 </script>
 
+<style>
+  :global(body) {
+    background-color: #dff5f6;
+    color: #091e20;;
+    transition: background-color 0.3s;
+  }
+  :global(body.dark-mode) {
+    background-color: #091f20;
+    color: #dff4f6;
+  }
+</style>
 
-<Nav />
-<main>
-  <FetchBudget />
-  <Button text="Primär Knapp" variant="primary" />
-  <Button text="Sekundär Knapp" variant="pumpkin"/>
-  <Button text="Primär Knapp" variant="yellow" />
-  <Button text="Sekundär Knapp" variant="pink"/>
-  <Button text="Primär Knapp" variant="green" />
-  <Button text="Sekundär Knapp" variant="purple"/>
+  <main>
+    <Nav />
+    <section>
+      <h1>detta är ett test!</h1>
+    <Button variant="primary">Toggle</Button>
+    <Button variant="secondary">Toggle</Button>
+  </section>
 </main>
+
+
