b06c7e6d297925971e8b685c3681079aef143c9e
Gr2dd3 <gradde23@gmail.com>
2023-11-27 16:32:36 +0100

    Added button to display sum of item.amount/category. All buttons display same value but calculates correct value of button pressed

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,16 +1,25 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  import FetchBudget from './Components/FetchBudget.svelte'
-  import Button from './Components/Button.svelte'
-  import Nav from './Components/Nav.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import FetchBudget from "./Components/FetchBudget.svelte";
+  import Button from "./Components/Button.svelte";
+  import Nav from "./Components/Nav.svelte";
 </script>
+
+<main>
+  <Nav />
+  <section>
+    <FetchBudget />
+    <Button variant="primary">Toggle</Button>
+    <Button variant="secondary">Toggle</Button>
+  </section>
+</main>
 
 <style>
   :global(body) {
     background-color: #dff5f6;
-    color: #091e20;;
+    color: #091e20;
     transition: background-color 0.3s;
   }
   :global(body.dark-mode) {
@@ -18,14 +27,3 @@
     color: #dff4f6;
   }
 </style>
-
-  <main>
-    <Nav />
-    <section>
-      <h1>detta är ett test!</h1>
-    <Button variant="primary">Toggle</Button>
-    <Button variant="secondary">Toggle</Button>
-  </section>
-</main>
-
-
--- frontend/src/Components/Accordion.svelte
+++ frontend/src/Components/Accordion.svelte
@@ -7,13 +7,9 @@
 
   let { localBudget } = budget;
 
-  const FindItem = (category) => () => {
-    indexOf = budget.expenses.findIndex((cat) => cat.name == category);
-  };
-
   const AddItem = (category) => () => {
     let itemToAdd = { name: "New Item", totalAmount: 0 };
-    FindItem(category);
+    indexOf = budget.expenses.findIndex((cat) => cat.name == category);
     budget.expenses[indexOf].items = [
       ...budget.expenses[indexOf].items,
       itemToAdd,
@@ -34,12 +30,14 @@
     result = JSON.stringify(json);
   }
 
+  let displayAmount;
   function CalculateTotalAmount(category) {
     let totalAmount = 0;
-    category.totalAmount = 0;
     category.items.map((item) => {
       totalAmount += item.amount;
       category.totalAmount = totalAmount;
+      displayAmount = category.totalAmount;
+      console.log(category.totalAmount);
     });
   }
 </script>
@@ -50,27 +48,43 @@
       {#each budget.expenses as expense}
         <Panel>
           <Header>
-            <input bind:value={expense.name} /> - {expense.totalAmount}
+            <div class="header-wrapper">
+              <input class="category-header" bind:value={expense.name} />
+              <div class="category-header-amount">
+                {displayAmount} SEK
+              </div>
+              <button
+                class="header-btn"
+                on:click={() => {
+                  CalculateTotalAmount(expense);
+                }}>Calculate total Amount</button
+              >
+            </div>
           </Header>
-          {CalculateTotalAmount(expense)}
           {#each expense.items as item}
             <Content>
-              <input
-                class="expence-items--name"
-                type="text"
-                bind:value={item.name}
-              />
-              <input
-                class="expence-items--amount"
-                type="digit"
-                min="0"
-                bind:value={item.amount}
-              />
-              <div class="expence--currency">{currency}</div>
-            </Content>
+              <div class="items-wrapper">
+                <input
+                  class="expence-items--name"
+                  type="text"
+                  bind:value={item.name}
+                />
+
+                <input
+                  class="expence-items--amount"
+                  type="digit"
+                  min="0"
+                  bind:value={item.amount}
+                />
+                <div class="expence--currency">{currency}</div>
+                <div />
+              </div></Content
+            >
           {/each}
-          <button style="margin: 20px;" on:click={AddItem(expense.name)}
-            >Add item</button
+          <button
+            class="add-item-btn"
+            style="margin: 20px;"
+            on:click={AddItem(expense.name)}>Add item</button
           >
         </Panel>
       {/each}
@@ -78,12 +92,60 @@
   </div>
 </main>
 
-<button on:click={() => PostBudgetToApi(budget)}>Post Budget</button>
+<div class="post-budget-wrapper">
+  <button class="post-budget-btn" on:click={() => PostBudgetToApi(budget)}
+    >Post Budget</button
+  >
+</div>
 
 <style>
-  .expence--items {
+  .header-wrapper {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 1rem;
+  }
+
+  .header-btn {
+    padding: 0.4rem;
+  }
+  .accordion-container {
+    margin-top: 4rem;
+  }
+
+  .category-header {
+    font-size: 1.5rem;
+    font-weight: 1.25rem;
+  }
+
+  .category-header-amount {
+    font-size: 1.5rem;
+    background-color: white;
+    padding: 2px 1rem 2px 1rem;
+    border: 1px solid darkgray;
+    width: max-content;
+  }
+
+  .add-item-btn {
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    border-radius: 5px;
+  }
+  .post-budget-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 1rem;
+  }
+
+  .post-budget-btn {
     display: inline-block;
-    padding: 1px;
+    font-size: 1.5rem;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    border-radius: 5px;
+    margin: 2rem 0 1rem 0;
   }
 
   .expence-items--name {
@@ -92,13 +154,16 @@
     margin: 0 1rem 0 0;
   }
 
+  .items-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
   .expence-items--amount {
-    display: inline-block;
-    width: 50px;
-    margin: 0 0.5rem 0 0;
+    width: auto;
   }
 
   .expence--currency {
-    display: inline-block;
   }
 </style>
