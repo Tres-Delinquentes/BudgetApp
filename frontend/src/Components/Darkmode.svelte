d871b8fefd27b1c525f4cf9704d7b33d421b3750
Jesper Misiorny <jesper.keden@hotmail.com>
2023-11-28 14:50:23 +0100

    Renamed button to darkmode, will fix SVG later and add new component for button

---
+++ frontend/src/Components/Darkmode.svelte
@@ -0,0 +1,49 @@
+<script>
+  export let variant = "primary";
+  function toggle() {
+    window.document.body.classList.toggle('dark-mode')
+  }
+</script>
+
+
+<style>
+  button {
+    padding: 10px 20px;
+    background-color: #32a8ae;
+		color: #091e20;
+		border: none;
+    border-radius: 12px;
+    cursor: pointer;
+    font-family: var(--ff-poppins);
+    font-size: var(--fs-200);
+    font-weight: 400;
+
+  }
+
+  .primary {
+    background-color: #32a8ae;
+		color: #091e20;
+  }
+
+  :global(body.dark-mode) .primary {
+    background-color: #51c6cd;
+    color: #dff4f6;
+  }
+
+  .secondary {
+    background-color: #bce8eb;
+    color: #091e20;
+  }
+  :global(body.dark-mode) .secondary {
+    background-color: #144043;
+    color: #dff4f6;
+}
+
+</style>
+
+
+<button class={variant} on:click={toggle}>
+  <slot/>
+</button>
+
+
