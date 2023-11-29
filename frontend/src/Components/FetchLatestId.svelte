eb0e8fec0f481fa1818b861bde2d84b93f031aae
Andreas Tollmar <andreastollmar@gmail.com>
2023-11-29 16:28:08 +0100

    fixed bugg with removing new items/id of items

--- frontend/src/Components/FetchLatestId.svelte
+++ frontend/src/Components/FetchLatestId.svelte
@@ -4,10 +4,13 @@
   export function GetLatestIdOfItem(budget) {
     budget.expenses.forEach((expense) => {
       expense.items.forEach((item) => {
-        if (item.id > itemId) itemId = item.id + 1;
+        if (item.id > itemId) {
+          itemId = item.id;
+        }
       });
     });
-
+    itemId++;
+    console.log(itemId);
     return itemId;
   }
 </script>
