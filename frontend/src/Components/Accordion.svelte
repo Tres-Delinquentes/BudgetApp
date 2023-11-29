f22dca6821cb2a68caf0ff2c9dae5eee4996c30a
Andreas Tollmar <andreastollmar@gmail.com>
2023-11-29 14:53:37 +0100

    added main to the html tree in accordion

--- frontend/src/Components/Accordion.svelte
+++ frontend/src/Components/Accordion.svelte
@@ -23,15 +23,15 @@
     console.log(budget);
   };
 
-    const RemoveItem = (categoryName, itemName) => () => {
-      var categoryIndex = localBudget.expenses.findIndex((cat) => cat.name == categoryName);
-      var itemIndex = localBudget.expenses.findIndex((item) => item.name == itemName);
-        localBudget.expenses[categoryIndex].items.splice(itemIndex, 1);
-  }
-
-
-
-
+  const RemoveItem = (categoryName, itemName) => () => {
+    var categoryIndex = localBudget.expenses.findIndex(
+      (cat) => cat.name == categoryName
+    );
+    var itemIndex = localBudget.expenses.findIndex(
+      (item) => item.name == itemName
+    );
+    localBudget.expenses[categoryIndex].items.splice(itemIndex, 1);
+  };
 
   async function PostBudgetToApi(budget) {
     fetch("https://localhost:7022/api/Budget", {
@@ -92,7 +92,6 @@
     0
   );
 
-
   //Nya accordion
   let openAccordionIndex = null;
 
@@ -105,10 +104,14 @@
   }
 </script>
 
-<main class="wrapper">
-    <div class="content-first"> 
+<main>
+  <div class="wrapper">
+    <div class="content-first">
       {#each budget.expenses as expense, index}
-        <div class="accordion-header subdisplay mb-3 mt-3" on:click={() => toggleAccordion(index)}>
+        <div
+          class="accordion-header subdisplay mb-3 mt-3"
+          on:click={() => toggleAccordion(index)}
+        >
           <span>{expense.name} - {expense.totalAmount}</span>
 
           {#if openAccordionIndex === index}
@@ -119,55 +122,75 @@
         </div>
 
         {#if openAccordionIndex === index}
-        <div class="accordion-content-first">
-          <p class="accordion-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste veritatis inventore repellat recusandae minus itaque, a porro similique soluta facilis non omnis laudantium, impedit eum fugit? Vitae veniam sint quidem!</p>
-        </div>
+          <div class="accordion-content-first">
+            <p class="accordion-paragraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              veritatis inventore repellat recusandae minus itaque, a porro
+              similique soluta facilis non omnis laudantium, impedit eum fugit?
+              Vitae veniam sint quidem!
+            </p>
+          </div>
           {#each expense.items as item}
             <div class="accordion-wrapper mt-2">
               <div class="accordion-content-first">
                 <!-- Ändra till delete istället för add på ikonen under denna rad-->
-                <button class="icon-button" on:click={RemoveItem(expense.name, item.name)}>
+                <button
+                  class="icon-button"
+                  on:click={RemoveItem(expense.name, item.name)}
+                >
                   <img src={Cross} class="item-icons" alt="delete itemfield" />
                 </button>
-              <input class="accordion-item-name" type="text" bind:value={item.name}/>
-              <input class="accordion-item-amount mx-2" type="number" min="0" bind:value={item.amount}/>
+                <input
+                  class="accordion-item-name"
+                  type="text"
+                  bind:value={item.name}
+                />
+                <input
+                  class="accordion-item-amount mx-2"
+                  type="number"
+                  min="0"
+                  bind:value={item.amount}
+                />
               </div>
             </div>
           {/each}
-            <div class="accordion-full-bleed">
-              <button class="icon-button mt-4" on:click={AddItem(expense.name)}>
-                <img src={Add} class="item-icons" alt="Add item" />
-                <p class="small-p">Add new field</p>
-              </button>
-            </div>  
+          <div class="accordion-full-bleed">
+            <button class="icon-button mt-4" on:click={AddItem(expense.name)}>
+              <img src={Add} class="item-icons" alt="Add item" />
+              <p class="small-p">Add new field</p>
+            </button>
+          </div>
         {/if}
-    {/each}
-  </div>
-  <div class="content-second">
-    <div class="wrapper-small">
-      <div class="small-left">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus
-        obcaecati possimus sequi aliquid eveniet labore, deleniti, quas rerum
-        sint optio nostrum, minima porro placeat. Minima sunt dignissimos saepe
-        iusto aut!
-      </div>
-      <div class="small-right">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit,
-        culpa eum ducimus fugit, modi magnam ipsum, consequuntur officia est
-        expedita quasi aliquam? Obcaecati minima sit accusantium atque omnis
-        perferendis expedita?
-      </div>
+      {/each}
     </div>
-    <div class="full-bleed subdisplay total-expenses">
-      Totala inkomster: {totalAmountExpense}kr<br />
-      Totala utgifter: {totalAmountExpense}kr
-    </div> 
-        <button on:click={() => PostBudgetToApi(budget)}>Post Budget</button> 
-    </div>  
+    <div class="content-second">
+      <div class="wrapper-small">
+        <div class="small-left">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Necessitatibus obcaecati possimus sequi aliquid eveniet labore,
+          deleniti, quas rerum sint optio nostrum, minima porro placeat. Minima
+          sunt dignissimos saepe iusto aut!
+        </div>
+        <div class="small-right">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit,
+          culpa eum ducimus fugit, modi magnam ipsum, consequuntur officia est
+          expedita quasi aliquam? Obcaecati minima sit accusantium atque omnis
+          perferendis expedita?
+        </div>
+      </div>
+      <div class="full-bleed subdisplay total-expenses">
+        Totala inkomster: {totalAmountExpense}kr<br />
+        Totala utgifter: {totalAmountExpense}kr
+      </div>
+      <button on:click={() => PostBudgetToApi(budget)}>Post Budget</button>
+      {#if errorResponse !== null}
+        <p>{errorResponse.message}</p>
+      {/if}
+    </div>
   </div>
 </main>
 
-<style>  
+<style>
   .accordion-header {
     display: flex;
     justify-content: space-between;
