969725cda6886e5e7c0988f5c3fa8bda7f49506e
Andreas Tollmar <andreastollmar@gmail.com>
2023-11-30 13:07:32 +0100

    start of income frontend accordion

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,23 +1,22 @@
 <script>
-  import FetchBudget from './Components/FetchBudget.svelte'
-  import Nav from './Components/Nav.svelte'
-  import Accordion from './Components/Accordion.svelte';
+  import FetchBudget from "./Components/FetchBudget.svelte";
+  import Nav from "./Components/Nav.svelte";
+  import Accordion from "./Components/Accordion.svelte";
+  import Income from "./Components/Income.svelte";
   let budget = { expenses: [], income: [] };
 </script>
-
-<style>
-
-</style>
 
 <main class="wrapper">
   <Nav />
   <div class="wrapper">
     <div class="content-first">
-      <Accordion { budget }/>
+      <Accordion {budget} />
     </div>
-    <div class="content-second">
-  
-    </div>
+    <div class="content-second"></div>
+    <Income {budget} />
   </div>
-  <FetchBudget bind:budget/>
+  <FetchBudget bind:budget />
 </main>
+
+<style>
+</style>
