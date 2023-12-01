aa6b8d684a9f02cd922303bd594778f2277a0a57
Jesper Misiorny <jesper.keden@hotmail.com>
2023-12-01 14:31:09 +0100

    working on main nav component

--- frontend/src/Components/Nav.svelte
+++ frontend/src/Components/Nav.svelte
@@ -4,49 +4,85 @@
   export let budgetTitle = "Name of budget";
 </script>
 
-<nav class="full-bleed">
+<div class="full-bleed">
+  <nav class="navbar">
+    <div class="navbar-content">
+      <h1 class="display">Budget Appen</h1>
+        <p class="p">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non in eum
+          distinctio libero adipisci, officia maxime aliquam corrupti iusto architecto
+          amet eveniet harum quis quasi sed corporis nemo excepturi quam!
+        </p>
+      <div class="column-container">
+        <div class="instructions mt-3">
+            <h2>Hur man använder Budgetappen</h2>
+              <ol>
+                <li>
+                  <strong>Välj en Mall</strong>
+                  <br>
+                    Börja med att välja ett av våra fördefinierade mallar eller skapa din egen budgetmall.
+                  </li>
+                <li>
+                  <strong>Fyll i Din Inkomst</strong> 
+                  <br>
+                    Kom ihåg att inkludera alla dina inkomster, inte bara din lön, utan även andra källor som bidrag, extrajobb, och passiva inkomster.
+                </li>
+                <li>
+                  <strong>Registrera Dina Utgifter</strong> 
+                  <br>
+                    Här kan du lägga till olika utgiftskategorier för att organisera dina kostnader. Exempel på kategorier kan vara 'Boende', 'Mat', 'Transport', 'Fritid', etc.
+                </li>
+                <li>
+                  <strong>Lägg till Utgiftskategorier</strong> 
+                  <br>
+                    Du kan enkelt lägga till nya utgiftskategorier genom att använda 'Lägg till ny kategori'-knappen. Detta ger dig möjligheten att anpassa din budget efter dina unika behov och utgiftsmönster.
+                </li>
+                <li>
+                  <strong>Anpassa och Uppdatera</strong> 
+                  <br>
+                    När du har lagt till dina inkomster och utgifter, kan du enkelt justera belopp och kategorier vid behov. Din totala budget kommer att uppdateras automatiskt för att reflektera dessa förändringar.
+                </li>
+              </ol>
+            <p>Genom att följa dessa enkla steg kan du skapa och hantera en budget som hjälper dig att hålla koll på dina finanser och planera för framtiden.</p>
+          </div>
+        </div>
+      </div>
+  </nav>
+</div>
 
-  <h1 class="display">Budget Appen</h1>
-  <p class="p">
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non in eum
-    distinctio libero adipisci, officia maxime aliquam corrupti iusto architecto
-    amet eveniet harum quis quasi sed corporis nemo excepturi quam!
-  </p>
-  <input class="budget-title" type="text" bind:value={budgetTitle} />
-  <h1 class="subdisplay">Välj ett av våra 3 templates</h1>
-  <button on:click={() => (budgetToDisplay = 0)}>1</button>
-  <button on:click={() => (budgetToDisplay = 1)}>2</button>
-  <button on:click={() => (budgetToDisplay = 2)}>3</button>
-
-
-
-
-
-    <div class="content-first">
-
-    
-    <div class="instructions mt-3">
-      <h2>Hur man använder Budgetappen</h2>
-      <ol>
-        <li><strong>Välj en Mall:</strong> Börja med att välja ett av våra fördefinierade mallar eller skapa din egen budgetmall.</li>
-        <li><strong>Fyll i Din Inkomst:</strong> Ange dina inkomstkällor. Kom ihåg att inkludera alla dina inkomster, inte bara din lön, utan även andra källor som bidrag, extrajobb, och passiva inkomster.</li>
-        <li><strong>Registrera Dina Utgifter:</strong> Fyll i dina utgifter. Här kan du lägga till olika utgiftskategorier för att organisera dina kostnader. Exempel på kategorier kan vara 'Boende', 'Mat', 'Transport', 'Fritid', etc.</li>
-        <li><strong>Lägg till Utgiftskategorier:</strong> Du kan enkelt lägga till nya utgiftskategorier genom att använda 'Lägg till ny kategori'-knappen. Detta ger dig möjligheten att anpassa din budget efter dina unika behov och utgiftsmönster.</li>
-        <li><strong>Anpassa och Uppdatera:</strong> När du har lagt till dina inkomster och utgifter, kan du enkelt justera belopp och kategorier vid behov. Din totala budget kommer att uppdateras automatiskt för att reflektera dessa förändringar.</li>
-      </ol>
-      <p>Genom att följa dessa enkla steg kan du skapa och hantera en budget som hjälper dig att hålla koll på dina finanser och planera för framtiden.</p>
-    </div>
-  </div>
-
-</nav>
 
 <style>
-  
+
+  .navbar {
+    padding: 60px;
+    max-width: 100%;
+    background-color: var(--clr-primary);
+    color: var(--clr-text);
+    margin-bottom: 1rem;
+    font-family: "Poppins", sans-serif;
+  }
+
+  .navbar-content,
+  .column-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .column-container {
+    display: grid;
+    grid-template-columns: 1fr 3fr 1fr;
+    gap: 1rem;
+    margin-top: 1rem;
+  }
+
+
  .instructions {
-    font-family: Arial, sans-serif;
+    grid-column: 2;
     max-width: 800px;
     margin: 0 auto;
-    padding: 20px;
+    padding: 1rem;
     background-color: #f8f8f8;
     border-radius: 4px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
@@ -58,21 +94,16 @@
   }
 
   .instructions ol {
-    counter-reset: step-counter;
+
     list-style-type: none;
-    padding-left: 0;
+    padding-left: 1.5rem;
   }
 
   .instructions ol li {
-    counter-increment: step-counter;
+
     margin-bottom: 15px;
   }
 
-  .instructions ol li::before {
-    content: counter(step-counter) ". ";
-    font-weight: bold;
-    color: #091e20;
-  }
 
   .instructions p {
     text-align: center;
@@ -81,26 +112,12 @@
     color: #333;
   }
 
-  .budget-title {
-    border: none;
-    box-shadow: none;
-    background-color: aqua;
-  }
-  nav {
-    padding: 60px;
-    width: 100%;
-    background-color: var(--clr-primary);
-    color: var(--clr-text);
-    margin-bottom: 1rem;
-    font-family: "Poppins", sans-serif;
-  }
-
   h1 {
     font-size: var(--fs-900);
     font-weight: 800;
   }
 
   p {
-    color: white;
+    color: #091e20;
   }
 </style>
