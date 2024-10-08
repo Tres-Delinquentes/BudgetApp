fb92a3f589de9ecbcd2c573eb9c8f12ecf7e236f
Gr2dd3 <gradde23@gmail.com>
2024-10-08 12:40:09 +0200

    added new adress for API request

--- frontend/src/Components/Functions/FetchBudget.svelte
+++ frontend/src/Components/Functions/FetchBudget.svelte
@@ -1,14 +1,21 @@
 <script>
   import { onMount } from "svelte";
   export let budgetList;
-
+  //const tresAzureURL = "https://tres-budget.azurewebsites.net/api/Budget";
+  const baseAdress =
+    "https://myapi-bubycqeyg2awapd6.northeurope-01.azurewebsites.net";
+  const url = "/api/Budget";
   onMount(async () => {
-    const response = await fetch("https://tres-budget.azurewebsites.net/api/Budget");
-    if (response.ok) {
-      budgetList = await response.json();
-      console.log(budgetList);
-    } else {
-      console.error("Failed to fetch budget:", response.status);
+    try {
+      const response = await fetch(baseAdress + url);
+      if (response.ok) {
+        budgetList = await response.json();
+        console.log(budgetList);
+      } else {
+        console.error("Failed to fetch budget:", response.status);
+      }
+    } catch (error) {
+      console.error("Error fetching budget:", error);
     }
   });
 </script>
